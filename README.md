20c1e3a6207d40953d3ab61899df736a00c4304c
jj espinoza <j.j.espinozagomez@gmail.com>
2021-04-15 19:34:07 -0500

    update

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # passwords-
 homework 3 JS
+
+
+first thing i did was to make a variable  "bank" for the numbers, letters, and so on. and also made blank bank for the characters to go once they were selected.
+
+next i tried to set up a way for the user to pick the set of characters they wanted. 
+
+i wrote out the 'if' statements to pass them off as true of false.  
+
+after that was to make a variable for each character case and prompt to confirm the variable. 
+
+after that was the most difficult for me, i tried with little to no success to set the range of characters from 8-120. most of my time went to trying to figure that out but i gave up. 
+
--- script.js
+++ script.js
@@ -1,68 +1,52 @@
  // Assignment Code
- var generateBtn = document.querySelector("#generate");  
-
-function generatePassword() {
-  var pass = ''
-  var passLength = 10;
-  for (let i = 0; i < passLength; i++) {
-    // console.log('picking a letter!!', lowerBank[Math.floor(Math.random() * lowerBank.length) ]);
-    pass += availableBank[Math.floor(Math.random() * availableBank.length) ]
-    // pass += upperBank[Math.floor(Math.random() * upperBank.length) ] 
-    // pass += specialBank[Math.floor(Math.random() * specialBank.length) ] 
-    // pass += numericBank[Math.floor(Math.random() * numericBank.length) ]
-  }
-  console.log('Password ???', pass)
-  return pass  
-
-  
-} 
+var generateBtn = document.querySelector("#generate");  
 var lowerBank = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperBank = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var specialBank = ['!','@','#','$','%','^', '&', '*', '(', ')', '{', '}', '+', '=', '<', '>', '/'];
 var numericBank = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 var availableBank = []; 
+
+function generatePassword(passLength) {
+  var pass = ''
+  
+  for (let i = 0; i < passLength; i++) {
+
+    randomBank = availableBank[Math.floor(Math.random() * availableBank.length) ]
+    pass += randomBank[Math.floor(Math.random() * randomBank.length) ] 
+  }
+  console.log('Password ???', pass)
+  return pass  
+} 
+
   
 // Write password to the #password input
 function writePassword() {
   
   var passwordText = document.querySelector("#password");
-
-  //passwordText.value = password;
-
   alert("let's make you a password!")  
   var length = parseInt(prompt('please enter the length of the password between 8-120 characters')); 
   
   if (length < 8) { 
-    console.log("password is too short");
     alert("password is too short")
+    return;
   } 
   
   if (length > 120) {
-    console.log("password is too long"); 
-    alert("password is too long")
+    alert("password is too long") 
+    return;
   }
   
 
-  console.log('logging available bank here ', availableBank)
-  console.log(lowerBank)
   var Lower = confirm('do you want a lower case?');
-  console.log('lower case ?', Lower)  
-  
+ 
   var Upper = confirm('do you want an upper case?'); 
-  console.log('upper case ?', Upper)
-  
+
   var Special = confirm('would you like a special character?'); 
-  console.log('special character  ?', Special)
   
   var Num = confirm('how about a number?'); 
-  console.log('number ?', Num)
-  
-  
   
   if(Lower === true){
-    
     availableBank = availableBank.concat(lowerBank);
-    console.log('here is my available bank', availableBank)
   }
   
   if(Upper === true){
@@ -76,16 +60,10 @@ function writePassword() {
   if(Num === true){
     availableBank = availableBank.concat(numericBank)
   }
-  console.log('Now this is the available bank that is correct',availableBank);
 
-  var password = generatePassword();
   //Now at this point, the computer has available bank to pick from 
-
-   passwordText.value = generatePassword();
+   passwordText.value = generatePassword(length);
 
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword); 
-
-
-
